b4026ab39f040419ae49dabf2872da9b42032a3a
rzhanna <zr28@cornell.edu>
2015-08-22 17:46:46 -0400

    Added comments to the cachematrix.r file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In general, the makeCacheMatrix function for storing a matrix as well as a 
+## list of 'special functions' associated with setting & retrieving the 
+## stored matrix and setting & retrieving the matrix's invserse.
 
-## Write a short comment describing this function
+## The cacheSolve() function takes the makeCacheMatrix() function/list as an 
+## input* and will yield either a cached value of the matrix's inverse
+## (if it was set beforehand), or will calculate the inverse, cache it, and 
+## return the inverse as an output.
+
+## *Note: This assumes the makeCacheMatrix was called initially with a matrix 
+## (e.g. >> makeCacheMatrix((matrix(1:3,nrow = 3)))), or the the stored matrix 
+## was set at a later time using the $set() function.
+
+## -----------------------------------------------------------------------------
+
+## The makeCacheMatrix takes a matrix as a formal argument, stores it, and 
+## creates a list of functions $set, $get, $setinverse, $getinverse for 
+## subsequently getting the matrix, setting/re-setting the matrix, setting a 
+## a cached version for the matrix's invserse , and then retrieving that cached 
+## inverse, respectively.
 
 makeCacheMatrix <- function(x = matrix()) {
       inv <- NULL
@@ -30,12 +46,13 @@ makeCacheMatrix <- function(x = matrix()) {
       )
 }
 
-
-## Write a short comment describing this function
+## The cacheSolve() function takes as an input the makeCacheMatrix() function/
+## list of functions (refered in the function as "mcm") and returns a matrix 
+## that is the INVERSE of the one stored in the makeCacheMatrix().
 
 cacheSolve <- function(mcm, ...) {
-       ## Return a matrix that is the inverse of 'x'
       inv <- mcm$getinverse()
+      
       if (!is.null(inv)) {
             message("getting cached inverse data")
             return(inv)
